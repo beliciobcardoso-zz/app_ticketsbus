132a7d4069b2474ab713befd0b471c6510ee7238
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-11-05 05:46:22 -0300

    traduçao do arquivo main a linha 12 a 20

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
+        // Esse é o tema do seu aplicativo.
         //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        // Tente executar seu aplicativo com "flutter run". Você verá o
+        // aplicativo possui uma barra de ferramentas azul. Então, sem sair do aplicativo, tente
+        // alterando o primarySwatch abaixo para Colors.green e depois invoque
+        // "hot reload" (pressione "r" no console em que você executou "flutter run",
+        // ou simplesmente salve suas alterações em "hot reload" em um Flutter IDE).
+        // Observe que o contador não foi redefinido para zero; a aplicação
+        // não é reiniciado.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
