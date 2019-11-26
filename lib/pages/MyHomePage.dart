e9398727b61f328ce8d86bce535cfd82df3e6652
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-11-26 06:13:36 -0300

    outros ajustes

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -1,6 +1,8 @@
 import 'package:app_ticketsbus/pages/AdicionarPassagens.dart';
 import 'package:flutter/material.dart';
 
+import 'Recarga.dart';
+
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,7 +13,14 @@ class MyHomePage extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.list),
             tooltip: 'Menu',
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Recarga(),
+                ),
+              );
+            },
           ),
         ],
       ),
--- lib/pages/Recarga.dart
+++ lib/pages/Recarga.dart
@@ -1,8 +1,116 @@
+import 'package:app_ticketsbus/models/MyTimeDate.dart';
+import 'package:app_ticketsbus/pages/Config.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:flutter/material.dart';
 
-class MyRecarga extends StatelessWidget {
+class Recarga extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var controller = new MoneyMaskedTextController(
+      decimalSeparator: ',',
+      thousandSeparator: '.',
+    );
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context, false),
+        ),
+        title: Text("Tickets Bus"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            tooltip: 'Configuração',
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyConfig(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 20,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            Text(
+              "Adicionar uma Recarga",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Data: $data",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 18,
+              ),
+            ),
+            TextFormField(
+              controller: controller,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Qual o valor da recargar?",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: TextStyle(fontSize: 40),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color(0xff3f51b5),
+                    Color(0xff3f51b5),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  child: Text(
+                    "Confirmar",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
