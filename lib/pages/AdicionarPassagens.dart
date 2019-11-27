08cfe1a70447227f7009dbfbc80f9f4d64774de5
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-11-26 23:28:43 -0300

    trabalhando na pagina add passagens

--- lib/pages/AdicionarPassagens.dart
+++ lib/pages/AdicionarPassagens.dart
@@ -19,81 +19,71 @@ class AdicionarPassagens extends StatelessWidget {
         title: Text("Tickets Bus"),
       ),
       body: Container(
-        padding: EdgeInsets.only(
-          top: 20,
-          left: 15,
-          right: 10,
-        ),
-        color: Colors.red[100],
-        child: ListView(
+        color: Colors.grey[300],
+        child: Column(
           children: <Widget>[
-            Text(
-              "Adicionar Passagem",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Data: $data",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 18,
-              ),
-            ),
-            TextFormField(
-              controller: quantpassagem,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Quantas Passagens uso hoje?",
-                labelStyle: TextStyle(
-                  backgroundColor: Colors.blue,
-                  color: Colors.black,
-                  fontSize: 25,
+            Container(
+              child: Text(
+                "Adicionar Passagens",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              style: TextStyle(fontSize: 40),
-            ),
-            SizedBox(
-              height: 15,
             ),
             Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.3, 1],
-                  colors: [
-                    Color(0xff3f51b5),
-                    Color(0xff3f51b5),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Text(
-                    "Confirmar",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 25,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  onPressed: () {},
-                ),
+              padding: EdgeInsets.only(top: 15),
+              //alignment: ,
+              child: Column(
+                children: <Widget>[
+                  Text("Data: $data"),
+                ],
               ),
             ),
+            //Inicio do inpunt "Quantas passagens uso hoje?"
+            Container(
+              //width: MediaQuery.of(context).size.width / 1.1,
+              padding: EdgeInsets.only(top: 25),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    //width: MediaQuery.of(context).size.width / 1.1,
+                    height: 70,
+                    padding: EdgeInsets.only(
+                      top: 1,
+                      right: 5,
+                      bottom: 1,
+                      left: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: TextFormField(
+                      controller: quantpassagem,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Quantas Passagens uso Hoje?",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
