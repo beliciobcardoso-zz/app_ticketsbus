951c70d3f4c9fc100d53ca297a3b49e7dd2b369a
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-11-30 06:22:05 -0300

    Finalizado Page Recarga

--- lib/pages/Recarga.dart
+++ lib/pages/Recarga.dart
@@ -33,79 +33,134 @@ class Recarga extends StatelessWidget {
         ],
       ),
       body: Container(
-        padding: EdgeInsets.only(
-          top: 20,
-          left: 40,
-          right: 40,
-        ),
-        color: Colors.white,
-        child: ListView(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.grey[400],
+        child: Column(
           children: <Widget>[
-            Text(
-              "Adicionar uma Recarga",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              height: 30,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: AlignmentDirectional.center,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Text(
+                      "Adicionar uma Recarga",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Data: $data",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 18,
-              ),
-            ),
-            TextFormField(
-              controller: controller,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Qual o valor da recargar?",
-                labelStyle: TextStyle(
-                  color: Colors.black38,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 20,
-                ),
-              ),
-              style: TextStyle(fontSize: 40),
-            ),
-            SizedBox(
-              height: 15,
             ),
             Container(
-              height: 60,
-              alignment: Alignment.centerLeft,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.3, 1],
-                  colors: [
-                    Color(0xff3f51b5),
-                    Color(0xff3f51b5),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-              ),
-              child: SizedBox.expand(
-                child: FlatButton(
-                  child: Text(
-                    "Confirmar",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontSize: 25,
+              margin: EdgeInsets.only(top: 5),
+              height: 30,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: AlignmentDirectional.centerStart,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Text(
+                      "Data: $data ",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                  onPressed: () {},
-                ),
+                ],
+              ),
+            ),
+
+            Container(
+              //Inicio do inpunt "Quantas passagens uso hoje?"
+              margin: EdgeInsets.only(top: 10),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    height: 70,
+                    padding: EdgeInsets.only(
+                      top: 1,
+                      right: 5,
+                      bottom: 1,
+                      left: 20,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                      color: Colors.white70,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: TextFormField(
+                      controller: controller,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "Qual o valor da recargar?",
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 19,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            //fim do inpunt "Quantas passagens uso hoje?"
+            Container(
+              //Container botão confirmar?"
+              margin: EdgeInsets.only(top: 25),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: AlignmentDirectional.center,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    height: 70,
+                    padding: EdgeInsets.only(
+                      top: 1,
+                      bottom: 1,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                      color: Colors.indigo,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: SizedBox.expand(
+                      child: FlatButton(
+                        child: Text(
+                          "Confirmar",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 25,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
