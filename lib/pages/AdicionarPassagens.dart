7630e256266034409e87e80a1217026f52baf0a6
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-11-30 05:53:19 -0300

    finalizado a page add passagens

--- lib/pages/AdicionarPassagens.dart
+++ lib/pages/AdicionarPassagens.dart
@@ -1,5 +1,4 @@
 import 'package:app_ticketsbus/models/MyTimeDate.dart';
-import 'package:app_ticketsbus/pages/Config.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:flutter/material.dart';
 
@@ -19,35 +18,56 @@ class AdicionarPassagens extends StatelessWidget {
         title: Text("Tickets Bus"),
       ),
       body: Container(
-        color: Colors.grey[300],
+        width: MediaQuery.of(context).size.width,
+        color: Colors.grey[400],
         child: Column(
           children: <Widget>[
             Container(
-              child: Text(
-                "Adicionar Passagens",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 15),
-              //alignment: ,
-              child: Column(
-                children: <Widget>[
-                  Text("Data: $data"),
-                ],
-              ),
-            ),
-            //Inicio do inpunt "Quantas passagens uso hoje?"
-            Container(
-              //width: MediaQuery.of(context).size.width / 1.1,
-              padding: EdgeInsets.only(top: 25),
+              margin: EdgeInsets.only(top: 10),
+              height: 30,
               child: Column(
                 children: <Widget>[
                   Container(
-                    //width: MediaQuery.of(context).size.width / 1.1,
+                    alignment: AlignmentDirectional.center,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Text(
+                      "Adicionar Passagens",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5),
+              height: 30,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: AlignmentDirectional.centerStart,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Text(
+                      "Data: $data ",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              //Inicio do inpunt "Quantas passagens uso hoje?"
+              margin: EdgeInsets.only(top: 10),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width / 1.1,
                     height: 70,
                     padding: EdgeInsets.only(
                       top: 1,
@@ -59,7 +79,7 @@ class AdicionarPassagens extends StatelessWidget {
                       borderRadius: BorderRadius.all(
                         Radius.circular(50),
                       ),
-                      color: Colors.white,
+                      color: Colors.white70,
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black,
@@ -83,7 +103,51 @@ class AdicionarPassagens extends StatelessWidget {
                   )
                 ],
               ),
-            )
+            ),
+            //fim do inpunt "Quantas passagens uso hoje?"
+            Container(
+              //Container botão confirmar?"
+              margin: EdgeInsets.only(top: 25),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: AlignmentDirectional.center,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    height: 70,
+                    padding: EdgeInsets.only(
+                      top: 1,
+                      bottom: 1,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(50),
+                      ),
+                      color: Colors.indigo,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: SizedBox.expand(
+                      child: FlatButton(
+                        child: Text(
+                          "Confirmar",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 25,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
