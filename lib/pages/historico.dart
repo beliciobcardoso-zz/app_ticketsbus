625c472e48ec0321807934b7ed4a64b01044e363
Belicio Batista Cardoso <44432451+belloinfo@users.noreply.github.com>
2019-12-07 10:58:07 -0300

    Criada a Pagina historico de recaraga

---
+++ lib/pages/historico.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HistoricoRecarga extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Histórico de Recarga"),
+      ),
+    );
+  }
+}
